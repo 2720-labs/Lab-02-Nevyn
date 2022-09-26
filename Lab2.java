cc0272eee0bd9edb5ffff5b626d4a413b21a588f
Nevyn <nevyn.dsouza@uleth.ca>
2022-09-25 19:09:08 -0600

    main class created

---
+++ Lab2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+    
+    public class Lab2{
+        
+        public add(int a, int b){
+
+        }
+
+        public subtract(int a, int b){
+
+        }
+
+        public multiply(int a, int b){
+            
+        }
+        
+        
+        public static void main(String [] args){
+
+    }
+}
