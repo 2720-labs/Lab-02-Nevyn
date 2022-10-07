b3aeaa5c5a26f88e2df3279bd08fa00a05a9e332
Nevyn <nevyn.dsouza@uleth.ca>
2022-10-06 22:07:01 -0600

    created main function

--- Lab2.java
+++ Lab2.java
@@ -2,20 +2,34 @@ import java.util.*;
     
     public class Lab2{
         
-        public int add(int a, int b){
-
+        static int add(int a, int b){
+            int sum = a + b;
+            return sum;
         }
 
-        public int subtract(int a, int b){
-
+        static int subtract(int a, int b){
+            int diff = a - b;
+            return diff;
         }
 
-        public int multiply(int a, int b){
-
+        static int multiply(int a, int b){
+            int prod = a * b;
+            return prod;
         }
         
         
         public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter first number: ");
+            int a = in.nextInt();
+            System.out.println("Enter second number: ");
+            int b = in.nextInt();
+            int sum = add(a,b);
+            int diff = subtract(a, b);
+            int prod = multiply(a, b);
 
+            System.out.println(a + " + " + b + " = " + sum);
+            System.out.println(a + " - " + b + " = " + diff);
+            System.out.println(a + " * " + b + " = " + prod);
     }
 }
